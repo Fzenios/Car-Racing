ae24517091036b49e11627415d2abfcc1a949356
Fedon <92860822+Fzenios@users.noreply.github.com>
2021-11-29 21:32:59 +0200

    fixed car movement

--- Car Racing/Assets/PlayerControls.cs
+++ Car Racing/Assets/PlayerControls.cs
@@ -4,24 +4,39 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    public Vector3 PlayerVertical, PlayerHorizontal;
+    public Vector3 PlayerVertical, MovementRight, MovementLeft;
+    public float MoveSpeed, TurnSpeed;
     public KeyCode Right, Left, Front, Back;
+    Rigidbody PlayerRot;
 
     void Start()
     {
-        
+        PlayerRot = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if(Input.GetKey(Left))
-            GetComponent<Rigidbody>().velocity -= PlayerHorizontal;
-        if(Input.GetKey(Right))
-            GetComponent<Rigidbody>().velocity += PlayerHorizontal;
-        if(Input.GetKey(Front))
-            GetComponent<Rigidbody>().velocity += PlayerVertical;
-        if(Input.GetKey(Back))
-            GetComponent<Rigidbody>().velocity -= PlayerVertical;
+        {
+        PlayerRot.AddTorque(-Vector3.up * TurnSpeed);
+        }
 
+
+        if(Input.GetKey(Right))
+        {
+            PlayerRot.AddTorque(Vector3.up * TurnSpeed);
+            //PlayerRot.MoveRotation(PlayerRot.rotation * deltaRotation );
+        }
+        if(Input.GetKey(Front))
+        {
+        // Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //PlayerRot.MovePosition(transform.position + PlayerVertical); //velocity += PlayerVertical;
+            PlayerRot.AddRelativeForce(Vector3.forward * MoveSpeed);
+        }
+            
+        if(Input.GetKey(Back))
+            //GetComponent<Rigidbody>().velocity -= PlayerVertical;
+            PlayerRot.AddRelativeForce(-Vector3.forward * MoveSpeed);
     }
+    
 }
