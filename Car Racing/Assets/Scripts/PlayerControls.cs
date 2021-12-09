6f0d4b632a685212dda65fb20402f6f0702544d4
Fedon <92860822+Fzenios@users.noreply.github.com>
2021-12-09 20:28:03 +0200

    Fixed Level and Car Handling

--- Car Racing/Assets/Scripts/PlayerControls.cs
+++ Car Racing/Assets/Scripts/PlayerControls.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float MoveSpeed, TurnSpeed, CantTurn;
-    public KeyCode Right, Left, Front, Back;
+    public float MoveSpeed, BackSpeed, BreakSpeed, TurnSpeed, CantTurn;
+    public float Gear0, Gear1, Gear2, Gear3, Gear4;
+    public KeyCode Right, Left, Front, Back, Brake;
     Rigidbody PlayerRb;
     float distToGround;
     public Text SpeedTxt;
@@ -20,12 +21,14 @@ public class PlayerControls : MonoBehaviour
     }
     bool IsGrounded() 
     {
-        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 100.5f);
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0f);
     }
     void Update() 
     {
         SpeedCurrent = PlayerRb.velocity.magnitude * 3.6f;
         SpeedTxt.text = SpeedCurrent + " km/h";
+        AccelerationCheck();
+        RotateCheck();
     }
 
     void FixedUpdate()
@@ -45,9 +48,14 @@ public class PlayerControls : MonoBehaviour
         }    
         if(Input.GetKey(Back))
         {
-            PlayerRb.AddRelativeForce(-Vector3.forward * MoveSpeed);
+            PlayerRb.AddRelativeForce(-Vector3.forward * BackSpeed);
         }
-        
+        if(Input.GetKey(Brake))
+        {
+            MoveSpeed = BreakSpeed;
+            BackSpeed = BreakSpeed;
+        }
+
         /*Vector3 LocalVelocity = transform.InverseTransformDirection(PlayerRb.velocity);
         LocalVelocity.x = 0;
         PlayerRb.velocity = transform.TransformDirection(LocalVelocity);*/
@@ -61,6 +69,30 @@ public class PlayerControls : MonoBehaviour
         if(Input.GetKey(Right))
         {
             PlayerRb.AddTorque(Vector3.up * TurnSpeed);
+        }
+    }
+    void AccelerationCheck()
+    {
+        if(!Input.GetKey(Brake))
+        {
+            if(SpeedCurrent < 170)
+                MoveSpeed = Gear1;
+            if(SpeedCurrent > 170)
+                MoveSpeed = Gear2;
+
+            BackSpeed = Gear0;
+        }
+    }
+    void RotateCheck()
+    {       
+        if(transform.rotation.eulerAngles.z > 88 && transform.rotation.eulerAngles.z < 180 )
+        {
+            transform.Rotate(0,0,-50f);
+            
+        }
+        if(transform.rotation.eulerAngles.z > 181 && transform.rotation.eulerAngles.z <= 272 )
+        {
+            transform.Rotate(0,0,50); 
         }
     }
     
